384cd683c677c307614c71a7922153ed9af102b3
tufedtm <tufedtm@gmail.com>
2016-03-05 08:33:31 +0300

    init

---
+++ forcier_bissex_chun/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for forcier_bissex_chun project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "forcier_bissex_chun.settings")
+
+application = get_wsgi_application()
