dcc2ef766bfa45cc1d66b3ca8ce0f6dce3e5c4e0
tufedtm <tufedtm@gmail.com>
2016-03-05 10:26:18 +0300

    *улучшил шаблон +упорядочивание по дате в модели BlogPost +форматирование даты в шаблоне

--- ch2_blog/models.py
+++ ch2_blog/models.py
@@ -7,3 +7,6 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=150)
     body = models.TextField()
     timestamp = models.DateTimeField()
+
+    class Meta:
+        ordering = ['-timestamp']
